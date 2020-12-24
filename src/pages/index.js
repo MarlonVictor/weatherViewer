037f246f251982309b668657b138e942cafade11
MarlonVictor <cmarlonvictor11@gmail.com>
2020-12-23 23:58:16 -0200

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Weather Viewer</title>
+                <link rel="icon" href="https://www.flaticon.com/svg/static/icons/svg/3937/3937315.svg" />
+            </Head>
+            <main>
+                <h1>weatherViewer</h1>
+            </main>
+        </div>
+    )
+}
